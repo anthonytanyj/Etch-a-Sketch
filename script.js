b671e2160f9dea63e18c00678363eeddb10e5a97
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-17 09:32:56 +0800

    Added Eraser, Rainbow and Clear button function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const box = document.createElement("div");
+const main = document.querySelector(".main");
 box.id = "container";
-document.body.appendChild(box);
+main.appendChild(box);
 
 // Styles for the body to center the container
 document.body.style.height = "100vh"; // Full viewport height
@@ -18,6 +19,34 @@ container.style.justifyContent = "space-between";
 container.style.alignContent = "space-between";
 container.style.border = '2px solid black'
 
+//rainbow
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return `rgb(${r},${g},${b})`; // Collect all to a css color string
+}
+
+let isRainbowMode = false;
+
+document.querySelector(".Rainbow").addEventListener("click", function () {
+    isRainbowMode = !isRainbowMode; // Toggle the rainbow mode on and off
+});
+
+let isEraser = false;
+
+document.querySelector(".Eraser").addEventListener("click", function () {
+    isEraser = !isEraser; // Toggle the Eraser mode on and off
+});
+
+document.querySelector(".Clear").addEventListener("click", function () {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(function (singleBox) {
+        singleBox.style.backgroundColor = "white";
+    });
+});
+
+
 function generateBoxes(value) {
     // First, clear the container
     container.innerHTML = '';
@@ -33,10 +62,18 @@ function generateBoxes(value) {
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
         box.style.border = '1px solid black';
-        box.addEventListener("mouseenter", function() {
-            this.style.backgroundColor = "black";
+        box.addEventListener("mouseenter", function () {
+            if (isRainbowMode && !isEraser) {
+                this.style.backgroundColor = getRandomColor();
+            } else if (isEraser) {
+                this.style.backgroundColor = "white";
+            }
+            else {
+                this.style.backgroundColor = "black";
+            }
         });
-        
+
+
         // // OPTIONAL: Revert style when the mouse leaves (hover ends)
         // box.addEventListener("mouseleave", function() {
         //     this.style.backgroundColor = "white";
@@ -61,7 +98,7 @@ generateBoxes(parseInt(slider.value));
 
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
     updateDisplay(this.value);
     generateBoxes(parseInt(this.value));
 } 
